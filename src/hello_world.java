982f66d90a1895adf7c7992492f2cb4bc2359ecb
admin <vimalthomas.career@gmail.com>
2024-05-01 23:13:05 -0500

    initial commit

---
+++ src/hello_world.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+import java.awt.Point;
+
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("this is nice");
+        int a = 10;
+        int b = 3;
+        float c = (float)a/(float)b;
+        for (b=b;b<a;b++){
+            System.out.println("increment");
+        }
+
+
+
+
+    }
+}
