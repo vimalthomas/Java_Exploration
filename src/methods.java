695f97569e3ec25388fa3ab38491c3018ec20793
admin <vimalthomas.career@gmail.com>
2024-05-01 23:52:33 -0500

    methods

---
+++ src/methods.java
@@ -0,0 +1,16 @@
+public class methods {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 67;
+        int result = add(a,b);
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b){
+
+
+        int c = a+b;
+        return(c);
+
+    }
+}
